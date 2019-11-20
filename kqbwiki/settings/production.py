167c6bc617b2abaf3e8e383d2a7f47f17654c341
ianfitzpatrick1 <ianfitz@gmail.com>
2019-11-20 15:12:11 -0600

    Updated production settings file with database name

--- kqbwiki/settings/production.py
+++ kqbwiki/settings/production.py
@@ -2,3 +2,18 @@ from .base import *  # noqa: F403
 
 ALLOWED_HOSTS = ['killerqueenblack.wiki']
 DEBUG = False
+
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kqbwiki',
+        'USER': 'kqb',
+        'PASSWORD': get_secret('DB_PASSWORD'),  # noqa: F405
+        'HOST': 'kqb.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+                'init_command': 'SET storage_engine=INNODB'
+        }
+    }
+}
